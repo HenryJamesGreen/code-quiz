009dab5dfeb5759e8191f87cbcb96635340ff76b
HenryJamesGreen <henryjamesgreenmusic@gmail.com>
2023-01-23 12:35:40 +0000

    added sample questions

---
+++ assets/js/questions.js
@@ -0,0 +1,37 @@
+let questions = [
+  {
+    question: "Answer is A",
+    answerCorrect: "A",
+    answerWrong: "B",
+    answerWrong: "C",
+    answerWrong: "D",
+  },
+  {
+    question: "Answer is A",
+    answerCorrect: "A",
+    answerWrong: "B",
+    answerWrong: "C",
+    answerWrong: "D",
+  },
+  {
+    question: "Answer is A",
+    answerCorrect: "A",
+    answerWrong: "B",
+    answerWrong: "C",
+    answerWrong: "D",
+  },
+  {
+    question: "Answer is A",
+    answerCorrect: "A",
+    answerWrong: "B",
+    answerWrong: "C",
+    answerWrong: "D",
+  },
+  {
+    question: "Answer is A",
+    answerCorrect: "A",
+    answerWrong: "B",
+    answerWrong: "C",
+    answerWrong: "D",
+  },
+];
