f9d0c1fa1a00d5c491a3a43bce6f637719d511fa
HenryJamesGreen <henryjamesgreenmusic@gmail.com>
2023-02-20 18:28:04 +0000

    added score system

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -2,11 +2,15 @@ let quizStartButton = document.querySelector("#start");
 let startScreen = document.querySelector(".start");
 let questionScreen = document.querySelector("#questions");
 let questionTitle = document.querySelector("#question-title");
-let displayChoices = document.querySelector("#choices");
+let displayChoices = document.querySelector("#answerForm");
 let btn = document.querySelector(".button");
 
+
 let questionIndex = 0;
-let score = 0;
+let score = {
+  Correct: 0,
+  Wrong: 0,
+}
 let question = questions[questionIndex];
 let userOptions = question.choices;
 let selected;
@@ -22,46 +26,70 @@ quizStartButton.addEventListener("click", function () {
 });
 
 function renderQuestion() {
-
   const currentAnswers = questions[currentQuestion].answers;
   answerForm.innerHTML = "";
 
-  question.innerText = questions[currentQuestion].text;
+  questionTitle.innerText = questions[currentQuestion].text;
 
   currentAnswers.forEach((answer) => {
-const button = document.createElement("button");
-  })
+    const button = document.createElement("button");
+    button.name = "answer";
+    button.classList = "btn btn-primary";
+    button.textContent = answer.text;
 
+    button.addEventListener("click", function (event) {
+      event.preventDefault();
+      const selectedIndex = questions[currentQuestion].answers.findIndex(
+        (a) => a.text === this.textContent
+      );
+      questions[currentQuestion].answers[selectedIndex].value += 1;
+      
+      const selectedDataIndex =
+        questions[currentQuestion].answers[selectedIndex].dataIndex;
+      score[selectedDataIndex] +=
+        questions[currentQuestion].answers[selectedIndex].value;
+
+      currentQuestion++;
+      if (currentQuestion === questions.length) {
+        console.log("Your scores:", score);
+       // window.location.href = "results.html?scores=" + JSON.stringify(score);
+      } else {
+        renderQuestion();
+      }
+    });
+
+    answerForm.appendChild(button);
+  });
 
   //questionTitle.textContent = question.ask;
   //console.log(question.ask);
   //console.log(question.correct);
   //console.log(question.choices);
   //for (i = 0; i < userOptions.length; i++) {
-   // let btn = document.createElement("button");
-   // btn.textContent = userOptions[i];
+  // let btn = document.createElement("button");
+  // btn.textContent = userOptions[i];
   //  btn.classList.add("button");
-    //btn.addEventListener("click", choiceSelect);
-   // document.body.appendChild(btn);
+  //btn.addEventListener("click", choiceSelect);
+  // document.body.appendChild(btn);
 
-   // function choiceSelect(event) {
-      //let selected = event.target.textContent;
-    //  let answer = question.correct;
-    //  if (selected === answer) {
-    //    btn.setAttribute("style", "color:green");
-     // } else {
-      //  btn.setAttribute("style", "color:red");
-     // }
-     // if (questionIndex < questions.length) {
-      //  questions.shift();
-      //  console.log(questions);
-     //   nextQuestion();
-     // } else if (questionIndex === questions.length) {
-    //  }
-   // }
- // }
+  // function choiceSelect(event) {
+  //let selected = event.target.textContent;
+  //  let answer = question.correct;
+  //  if (selected === answer) {
+  //    btn.setAttribute("style", "color:green");
+  // } else {
+  //  btn.setAttribute("style", "color:red");
+  // }
+  // if (questionIndex < questions.length) {
+  //  questions.shift();
+  //  console.log(questions);
+  //   nextQuestion();
+  // } else if (questionIndex === questions.length) {
+  //  }
+  // }
+  // }
 }
 
-function nextQuestion() {
-  renderQuestion();
-}
+//function nextQuestion() {
+ // renderQuestion();
+//}
