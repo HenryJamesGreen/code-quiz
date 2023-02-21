6cd36006c2dea57e074e929c7e8ec784f5227d39
HenryJamesGreen <henryjamesgreenmusic@gmail.com>
2023-02-21 00:19:59 +0000

    added clear button

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,8 +1,14 @@
 let highScores = document.querySelector("#highscores");
+let clear = document.querySelector("#clear");
 
 Object.keys(localStorage).forEach(function (key) {
   console.log(`Key: ${key}, Value: ${localStorage.getItem(key)}`);
   const li = document.createElement("li");
   li.textContent = `${key}: ${localStorage.getItem(key)}`;
   highScores.appendChild(li);
+});
+
+clear.addEventListener("click", function () {
+  localStorage.clear();
+  highScores.innerHTML = " ";
 });
